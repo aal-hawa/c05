d859227ef168e486f7659f89c8ba21c946e77a88
Anas Al Hawamda <aal-hawa@lab1r3s10.42abudhabi.ae>
2025-09-01 12:54:54 +0400

    edit

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,30 +5,30 @@
 
 int main() {
 	try {
-		Bureaucrat ahmad("Ahmad", 1); // High rank: can sign and execute anything
-		Bureaucrat ali("Ali", 140);   // Can only sign simple forms
+		Bureaucrat ahmad("Ahmad", 1);
+		Bureaucrat ali("Ali", 140);
 
 
 		std::cout << "----------------Test ShrubberyCreationForm-------------------" << std::endl;
 	
 		ShrubberyCreationForm shrubbery("Garden");
 		std::cout << shrubbery << std::endl;
-		ali.signForm(shrubbery); // Should succeed
-		ahmad.executeForm(shrubbery); // Ahmad executes
+		ali.signForm(shrubbery);
+		ahmad.executeForm(shrubbery);
 
 		std::cout << "----------------Test RobotomyRequestForm---------------------" << std::endl;
 
 		RobotomyRequestForm robotomy("Bender");
 		std::cout << robotomy << std::endl;
-		ahmad.signForm(robotomy); // Ahmad signs
-		ahmad.executeForm(robotomy); // Ahmad executes
+		ahmad.signForm(robotomy);
+		ahmad.executeForm(robotomy);
 
 		std::cout << "----------------Test PresidentialPardonForm------------------" << std::endl;
 
 		PresidentialPardonForm pardon("Arthur Dent");
 		std::cout << pardon << std::endl;
-		ahmad.signForm(pardon); // Ahmad signs
-		ahmad.executeForm(pardon); // Ahmad executes
+		ahmad.signForm(pardon);
+		ahmad.executeForm(pardon);
 
 	} catch (const std::exception &e) {
 		std::cerr << "Exception: " << e.what() << std::endl;
